031cd132ec5cfd467d2e712aa47166abbe6fc2c2
yann <tmp4321@qq.com>
2018-03-16 16:14:32 +0800

    go cgroups test
    Signed-off-by: yann <tmp4321@qq.com>

--- 2.1/main.go
+++ 2.1/main.go
@@ -1,16 +1,25 @@
 package main
+
 import(
+"log"
+"os"
 "os/exec"
 "syscall"
-"os"
-"log"
 )
 
 func main() {
     cmd :=exec.Command("sh")
     cmd.SysProcAttr = &syscall.SysProcAttr{
-        Cloneflags: syscall.CLONE_NEWUTS,
+        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
     }
+    // cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+
+    cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{
+    {ContainerID: 5001, HostID: syscall.Getuid(), Size: 1},
+}
+cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{
+    {ContainerID: 5001, HostID: syscall.Getgid(), Size: 1},
+}
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
@@ -18,6 +27,8 @@ func main() {
     if err :=cmd.Run(); err !=nil{
         log.Fatal(err)
     }
+    os.Exit(-1)
 }
+
 
 
