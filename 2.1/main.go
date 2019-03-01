eba3fd33453103661050a2d297848912313f3198
yann <tmp4321@qq.com>
2019-03-01 13:56:31 +0800

    UTS Namespace
    Signed-off-by: yann <tmp4321@qq.com>

--- 2.1/main.go
+++ 2.1/main.go
@@ -8,17 +8,12 @@ import(
 )
 
 func main() {
-    cmd :=exec.Command("sh")
+    cmd :=exec.Command("sh")    // 新进程初始命令
     cmd.SysProcAttr = &syscall.SysProcAttr{
-        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
-    }
-    // cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+        Cloneflags: syscall.CLONE_NEWUTS,
+        // syscall.CLONE_NEWUTS 创建 UTS Namespace等
 
-    cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{
-    {ContainerID: 5001, HostID: syscall.Getuid(), Size: 1},
-}
-cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{
-    {ContainerID: 5001, HostID: syscall.Getgid(), Size: 1},
+    }
 }
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
@@ -27,7 +22,7 @@ cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{
     if err :=cmd.Run(); err !=nil{
         log.Fatal(err)
     }
-    os.Exit(-1)
+    // os.Exit(-1)
 }
 
 
