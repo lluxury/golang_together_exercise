5faa001458ca8d2c33c0ac3582a5d6e2144245d1
yann <tmp4321@qq.com>
2019-03-01 14:24:59 +0800

    IPC Namespace
    Signed-off-by: yann <tmp4321@qq.com>

--- 2.1/main.go
+++ 2.1/main.go
@@ -10,11 +10,11 @@ import(
 func main() {
     cmd :=exec.Command("sh")    // 新进程初始命令
     cmd.SysProcAttr = &syscall.SysProcAttr{
-        Cloneflags: syscall.CLONE_NEWUTS,
-        // syscall.CLONE_NEWUTS 创建 UTS Namespace等
-
+        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC,
     }
-    //}
+    // cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+    // syscall.CLONE_NEWUTS 创建 UTS Namespace等
+
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
@@ -22,7 +22,7 @@ func main() {
     if err :=cmd.Run(); err !=nil{
         log.Fatal(err)
     }
-    // os.Exit(-1)
+    os.Exit(-1)
 }
 
 
