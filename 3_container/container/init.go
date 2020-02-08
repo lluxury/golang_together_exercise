3b07be59e1536426ae3a773bc00a81aabe1cb1e2
yann <tmp4321@qq.com>
2020-02-08 02:47:16 -0800

    mydocker cgroup part 2
    Signed-off-by: yann <tmp4321@qq.com>

--- 3_container/container/init.go
+++ 3_container/container/init.go
@@ -1,21 +1,57 @@
 package container
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"strings"
 	"syscall"
 
 	"github.com/Sirupsen/logrus"
 )
 
 
-func RunContainerInitProcess(command string, args []string) error {
-	logrus.Infof("command %s", command)
+//func RunContainerInitProcess(command string, args []string) error {
+func RunContainerInitProcess() error {
+	//logrus.Infof("command %s", command)
+	cmdArray := readUserCommand()
+	if cmdArray == nil || len(cmdArray) == 0 {
+		return fmt.Errorf("Run container get user command error, cmdArray is nil")
+	}
 
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
-	argv :=[]string{command}
-	if err := syscall.Exec(command, argv, os.Environ()); err != nil{
+	//setUpMount()
+	path, err := exec.LookPath(cmdArray[0])
+	if err != nil {
+		logrus.Errorf("Exec loop path error %v", err)
+		return err
+	}
+	logrus.Info("Find path %s", path)
+
+	//defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	//syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	//argv :=[]string{command}
+
+	//if err := syscall.Exec(command, argv, os.Environ()); err != nil{
+	//	logrus.Errorf(err.Error())
+	//}
+
+	if err := syscall.Exec(path, cmdArray[0:], os.Environ()); err != nil {
 		logrus.Errorf(err.Error())
 	}
+
 	return nil
 }
+
+func readUserCommand() []string {
+	pipe := os.NewFile(uintptr(3), "pipe")
+	msg, err := ioutil.ReadAll(pipe)
+	if err != nil {
+		logrus.Errorf("init read pipe error %v", err)
+		return nil
+	}
+	msgStr := string(msg)
+	return strings.Split(msgStr, " ")
+}
+
+
--- 3_container/main_command.go
+++ 3_container/main_command.go
@@ -7,6 +7,7 @@ import (
 	"github.com/urfave/cli"
 	//"github.com/xianlubird/mydocker/container"
 	"github.com/lluxury/golang_together_exercise/3_container/container"
+	"github.com/lluxury/golang_together_exercise/3_container/cgroups/subsystems"
 )
 
 var runCommand = cli.Command{
@@ -17,15 +18,27 @@ var runCommand = cli.Command{
 							if len(context.Args()) < 1{
 								return fmt.Errorf("Missing container command")
 							}
-							cmd := context.Args().Get(0)
+							var cmdArray []string
+		for _, arg := range context.Args() {
+			cmdArray = append(cmdArray, arg)
+		}
+							//cmd := context.Args().Get(0)
 							tty := context.Bool("ti")
-							Run(tty, cmd)
+							resConf := &subsystems.ResourceConfig{
+								MemoryLimit: "m",
+							}
+							
+							Run(tty, cmdArray, resConf)
 							return nil
 	},
 	Flags:  []cli.Flag{
 		cli.BoolFlag{
 			Name:  "ti",
 			Usage: "enable tty",
+		},
+		cli.StringFlag{
+			Name: "m",
+			Usage: "memory limit",
 		},
 	},
 }
@@ -34,12 +47,12 @@ var runCommand = cli.Command{
 var initCommand = cli.Command{
 	Name:   "init",
 	Usage:  "Init container process run user's process in container. Do not call it outside",
-	// Action:  func(context cli.Context) error{
         Action: func(context *cli.Context) error{
 		log.Infof("init come on")
-		cmd := context.Args().Get(0)
-		log.Infof("command %s", cmd)
-		err := container.RunContainerInitProcess(cmd, nil)
+		// cmd := context.Args().Get(0)
+		// log.Infof("command %s", cmd)
+		// err := container.RunContainerInitProcess(cmd, nil) 逻辑错误
+		err := container.RunContainerInitProcess()
 		return err
 	},
 }
